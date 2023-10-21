99af5db8f2a8c8c3847cd7d49d09e5758aa8c1c1
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-21 08:08:57 -0300

    testsando

---
+++ PrimeiraApi/Controller/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiraApi.Controller
+{
+    [ApiController]
+    [Route("/api/v1/home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
