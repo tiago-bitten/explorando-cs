245ee21f7afe31b5dc5c88c86f98dbff42d86d9c
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-21 12:29:18 -0300

    criei usuario model

---
+++ AgoraVai/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public User(int id, string name, string email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
+}
