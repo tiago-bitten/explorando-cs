a690e9553c5f41f536ec5cecba3ebab6c955c1ec
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-22 06:02:17 -0300

    agora entendi melhor

---
+++ Tentando/Tentando/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tentando.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet(Name = "GetHello")]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
