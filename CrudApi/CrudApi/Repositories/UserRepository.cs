fb92195b594df64176b0d6a9db3896aa848dd3c3
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-23 13:09:21 -0300

    implements user repository interface

---
+++ CrudApi/CrudApi/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using CrudApi.Models;
+using CrudApi.Repositories.Interfaces;
+
+namespace CrudApi.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User> CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
