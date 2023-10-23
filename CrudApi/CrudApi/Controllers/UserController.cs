034d257e40e4ba1c51ef99f0e22636990182dc3f
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-23 12:51:57 -0300

    crud api

---
+++ CrudApi/CrudApi/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
