2c4dfc0d576da8faf24a1e21d91366a6be771c24
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-24 13:13:22 -0300

    post crud

--- CrudApi/CrudApi/Controllers/UserController.cs
+++ CrudApi/CrudApi/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CrudApi.Models;
+using CrudApi.Repositories;
+using CrudApi.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CrudApi.Controllers
 {
@@ -6,6 +9,23 @@ namespace CrudApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserRepository _userRepository;
 
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAll()
+        {
+            return await _userRepository.GetAllAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Create([FromBody] User user)
+        {
+            return await _userRepository.CreateAsync(user);
+        }
     }
 }
--- CrudApi/CrudApi/Program.cs
+++ CrudApi/CrudApi/Program.cs
@@ -1,4 +1,6 @@
 using CrudApi.Data;
+using CrudApi.Repositories;
+using CrudApi.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddDbContext<CrudApiContext>(options =>
 {
