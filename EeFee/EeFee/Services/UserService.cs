a84573a0ab0f5e3abb34e7ce7334213e767a93ad
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-28 23:38:51 -0300

    add scope

---
+++ EeFee/EeFee/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using EeFee.Models;
+using EeFee.Repositories.Interfaces;
+using EeFee.Services.Interfaces;
+
+namespace EeFee.Services
+{
+    public class UserService : IUserService
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> FindByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
