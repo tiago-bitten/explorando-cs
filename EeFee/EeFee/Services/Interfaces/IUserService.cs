53e627402447c12ff6cf3d5ce7fa64c58c5e7dba
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-28 23:37:17 -0300

    user interface

---
+++ EeFee/EeFee/Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using EeFee.Models;
+
+namespace EeFee.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task CreateAsync(User user);
+        Task DeleteAsync(User user);
+        Task UpdateAsync(User user);
+        Task<User> FindByIdAsync(int id);
+        Task<User> FindByUsernameAsync(string username);
+    }
+}
