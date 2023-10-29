59ab3613cae1c3309db3f046595543ec4bdd2a75
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-28 23:34:37 -0300

    user repository

---
+++ EeFee/EeFee/Repositories/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using EeFee.Models;
+using EeFee.Repositories.Interfaces;
+
+namespace EeFee.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task CreateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> FindByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
