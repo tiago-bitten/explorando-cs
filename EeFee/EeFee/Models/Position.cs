edc5c67b4cdb13ccc3be6ca4130551e671c5eeb5
BuildTools <tiago.sbittencourt@icloud.com>
2023-10-30 13:07:34 -0300

    add position

---
+++ EeFee/EeFee/Models/Position.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EeFee.Models
+{
+    [Table("tb_position")]
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
